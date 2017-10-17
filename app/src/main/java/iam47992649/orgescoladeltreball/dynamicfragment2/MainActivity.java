22187d7355c0fae5367047f26f884edb1d285a2b
lpbove <lpbove@gmail.com>
2017-10-17 18:30:44 +0200

    Created DynamicFragment repository

---
+++ app/src/main/java/iam47992649/orgescoladeltreball/dynamicfragment2/MainActivity.java
@@ -0,0 +1,13 @@
+package iam47992649.orgescoladeltreball.dynamicfragment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
